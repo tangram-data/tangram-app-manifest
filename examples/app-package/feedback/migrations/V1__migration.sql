d8d41866b8db8c50feefeb6eb1267eaeaa867861
Jian TANG <jtang1126@gmail.com>
2026-07-05 18:07:36 +0100

    Add helm chart CRDs

--- examples/app-package/feedback/migrations/V1__migration.sql
+++ examples/app-package/feedback/migrations/V1__migration.sql
@@ -1,2 +1,8 @@
-create table notes (id bigint not null, note text, done boolean, approved_by text, approved_at timestamptz, primary key (id));
+create table notes
+(id bigint not null,
+ note text,
+ done boolean,
+  approved_by text,
+  approved_at timestamptz,
+  primary key (id));
 create index notes_done_idx on notes (done);
